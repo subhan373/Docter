cba3e7ab561eaa02021e3fb730ac78c3fdb6d345
subhan373 <subhanwork132@gmail.com>
2025-10-23 15:08:23 +0500

    Hi

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -7,6 +7,8 @@ import { Link } from "react-router-dom";
 
 
 
+
+
 export default function Card() {
   return (
     <section
--- src/Components/Find.jsx
+++ src/Components/Find.jsx
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 
 
 
+
+
 const specialties = [
   {
     name: "General Physician",
