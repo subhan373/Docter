173a059f43e5d957d562bb93ccc016977973d2c9
subhan373 <subhanwork132@gmail.com>
2025-10-23 15:08:37 +0500

    Hi

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -9,6 +9,7 @@ import { Link } from "react-router-dom";
 
 
 
+
 export default function Card() {
   return (
     <section
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -8,6 +8,8 @@ import React from "react";
 
 
 
+
+
 const Footer = () => {
   return (
     <footer className="bg-white text-gray-700 mt-10">
