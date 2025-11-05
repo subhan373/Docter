4aabff8bd496c64d3f915223fcebab6f57e75068
subhan373 <subhanwork132@gmail.com>
2025-11-05 15:23:00 +0500

    Hi

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,11 +4,6 @@ import Navbar from "../Components/Nav";
 
 
 
-
-
-
-
-
 const AboutUs = () => {
   return (
     <>
--- src/pages/AllDoc.jsx
+++ src/pages/AllDoc.jsx
@@ -8,13 +8,6 @@ import DoctorFil from "../Components/DoctorFil";
 
 
 
-
-
-
-
-
-
-
 const doctors = [
   {
 
