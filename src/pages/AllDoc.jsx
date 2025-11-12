11d7abaf6a27f246d050bebd771a8235948f119d
subhan373 <subhanwork132@gmail.com>
2025-11-12 19:56:34 +0500

    Hi

--- src/pages/AllDoc.jsx
+++ src/pages/AllDoc.jsx
@@ -12,6 +12,9 @@ import DoctorFil from "../Components/DoctorFil";
 
 
 
+
+
+
 const doctors = [
   {
 
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -14,6 +14,13 @@ import Navbar from "../Components/Nav";
 
 
 
+
+
+
+
+
+
+
 const Contact = () => {
   return (
     <>
